2c776fdbadb5bd19cd2b395b1dc6e614bb7c8e5a
jp7086131-lang <jp7086131@gmail.com>
2026-05-31 18:23:35 +0800

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'motobook-pwa-v3';
+const OFFLINE_ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => caches.open(CACHE_NAME))
+      .then((cache) => cache.addAll(OFFLINE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+      )
+      .then(() => {
+        if (
+          self.location.hostname === 'localhost' ||
+          self.location.hostname === '127.0.0.1'
+        ) {
+          return self.registration.unregister();
+        }
+        return undefined;
+      })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('/index.html')))
+  );
+});
